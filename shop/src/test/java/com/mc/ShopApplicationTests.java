473516126d53963f824bdcb14e92e1d1250a97d9
skGang <foxnrose@gmail.com>
2025-03-27 02:08:34 +0900

    test, and slf4j setting

--- shop/src/test/java/com/mc/ShopApplicationTests.java
+++ shop/src/test/java/com/mc/ShopApplicationTests.java
@@ -1,14 +1,17 @@
 package com.mc;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class ShopApplicationTests {
 
     @Test
     void contextLoads() {
-        System.out.println("Hello Git");
+        log.error("Error Message   {}", new RuntimeException());
+        log.info("info Message   {}", new RuntimeException());
     }
 
 }
